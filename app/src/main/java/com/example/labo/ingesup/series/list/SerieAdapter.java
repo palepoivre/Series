1851fc3fcb629fece877619bc7ec9e97562b3ad2
Come <labordecome@gmail.com>
2014-11-05 17:11:24 +0100

    Correction erreur

--- app/src/main/java/com/example/labo/ingesup/series/list/SerieAdapter.java
+++ app/src/main/java/com/example/labo/ingesup/series/list/SerieAdapter.java
@@ -1,8 +1,14 @@
 package com.example.labo.ingesup.series.list;
 
 import android.content.Context;
+import android.content.Intent;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.example.labo.ingesup.series.R;
+import com.example.labo.ingesup.series.activities.DetailActivity;
 import com.example.labo.ingesup.series.bean.Genre;
 import com.example.labo.ingesup.series.bean.Serie;
 
@@ -24,5 +30,6 @@ public class SerieAdapter extends ArrayAdapter {
     public SerieAdapter(Context context, int resource, List objects, List<Serie> mSeries) {
         super(context, resource, objects);
         this.mSeries = mSeries;
+
     }
-}
+};
